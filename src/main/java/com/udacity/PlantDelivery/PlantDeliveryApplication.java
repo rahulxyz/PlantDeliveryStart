402910294bc7b5544ee5c0e532055a31bc048306
rahulxyz <rahulguptaxyz15@gmail.com>
2021-04-01 21:58:29 +0530

    boilerplate

---
+++ src/main/java/com/udacity/PlantDelivery/PlantDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.PlantDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udacity/PlantDelivery/PlantDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.PlantDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
